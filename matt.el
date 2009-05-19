c90448cfbed4a5b684a19d8af4350b88e9129800
Matt Henry <greenideas@gmail.com>
2009-05-19 14:12:09 -0400

    Added some user-specific configs

---
+++ matt.el
@@ -0,0 +1,14 @@
+;;; Custom configurations
+
+; Define fullscreen command
+(defun fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen (if (frame-parameter nil 'fullscreen)
+                                           nil
+                                           'fullboth)))
+(global-set-key [(meta return)] 'fullscreen)
+
+; Sync kill ring with OSX clipboard
+(global-set-key (kbd "C-w") 'clipboard-kill-region)
+(global-set-key (kbd "M-w") 'clipboard-kill-ring-save)
+(global-set-key (kbd "C-y") 'clipboard-yank)
